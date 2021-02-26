9cc1df7c1aab7de4f4524591677d5de4ae3be9ee
Missy Davies <ms.melissadavies@gmail.com>
2021-02-26 17:37:21 +0000

    Added function to parse customer data from a file

--- accounting.py
+++ accounting.py
@@ -1,61 +1,24 @@
-melon_cost = 1.00
+def has_customer_underpaid(melon_cost, path):
+    """Calculate whether the customer underpaid
+    
+    Given the price of melons and a file path, iterate through each line in the 
+    path and parse the data to determine if the customer has underpaid."""
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+    log = open(path) # open file 
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+    for line in log: # iterate through each line in the file 
+        customer_data = line.split("|") # tokenize the data into a list 
+        customer_number = customer_data[0] 
+        customer_name = customer_data[1]
+        customer_melons = customer_data[2]
+        customer_paid = float(customer_data[3])
+        
+        customer_expected = int(customer_melons) * melon_cost
+        if customer_expected != customer_paid:
+            print(f"{customer_name} paid ${customer_paid:.2f},", # print how much they underpaid if that's the case 
+            f"expected ${customer_expected:.2f}"
+            )
+    
+    log.close() # close file 
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
-
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
-
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
-
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
-
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
+has_customer_underpaid(melon_cost=1.00, path="customer-orders.txt")
