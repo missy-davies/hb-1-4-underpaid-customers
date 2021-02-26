2c11aba42b02896383dd96007091f3c98d57fe3a
Missy Davies <ms.melissadavies@gmail.com>
2021-02-26 18:20:40 +0000

    Refactored function to deal with customers who underpaid and overpaid

--- accounting.py
+++ accounting.py
@@ -1,4 +1,6 @@
-def has_customer_underpaid(melon_cost, path):
+MELON_COST = 1.00 # global variable for melon cost
+
+def paid_correct_amount(melon_cost, path):
     """Calculate whether the customer underpaid
     
     Given the price of melons and a file path, iterate through each line in the 
@@ -8,17 +10,18 @@ def has_customer_underpaid(melon_cost, path):
 
     for line in log: # iterate through each line in the file 
         customer_data = line.split("|") # tokenize the data into a list 
-        customer_number = customer_data[0] 
         customer_name = customer_data[1]
-        customer_melons = customer_data[2]
+        customer_melons = int(customer_data[2])
         customer_paid = float(customer_data[3])
         
-        customer_expected = int(customer_melons) * melon_cost
-        if customer_expected != customer_paid:
-            print(f"{customer_name} paid ${customer_paid:.2f},", # print how much they underpaid if that's the case 
-            f"expected ${customer_expected:.2f}"
-            )
+        customer_expected = customer_melons * melon_cost
+        if customer_expected > customer_paid:
+            print(f"{customer_name} has underpaid for their melons.") # print whether customer over/underpaid
+
+        elif customer_expected < customer_paid:
+            print(f"{customer_name} has overpaid for their melons.") # print whether customer over/underpaid
+
     
     log.close() # close file 
 
-has_customer_underpaid(melon_cost=1.00, path="customer-orders.txt")
+paid_correct_amount(melon_cost=MELON_COST, path="customer-orders.txt") # call the function
